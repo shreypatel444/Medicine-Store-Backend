e6d3e5daf0926bcbbb18a2d2ae73a63eff3c6b9b
SHREY PATEL <127281095+shreypatel444@users.noreply.github.com>
2025-04-08 12:48:43 +0530

    Update connection.js

--- connection/connection.js
+++ connection/connection.js
@@ -4,11 +4,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const URL = "mongodb+srv://smpatelshreypatel:rMMiF2gc5NeKlEDv@clusterpharmanet.o8erlsw.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPharmaNet";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || URL);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ Database Connection Error:", error);
